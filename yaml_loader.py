0917c720701b6c3a58efc83120fd652807863535
timlin <tilin@wish.com>
2022-11-24 14:04:39 +0800

    test

--- yaml_loader.py
+++ yaml_loader.py
@@ -1,12 +1,17 @@
 from airflow import DAG
 import os
-#import dagfactory
-# _cur_path = os.path.dirname(os.path.abspath(__file__))
+import dagfactory
+_cur_path = os.path.dirname(os.path.abspath(__file__))
 # #print(_cur_path)
-# dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"test.yaml"))
+dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"test.yaml"))
 
-# dag_factory.clean_dags(globals())
-# dag_factory.generate_dags(globals())
-from dagfactory import load_yaml_dags
+dag_factory.clean_dags(globals())
+dag_factory.generate_dags(globals())
 
-load_yaml_dags(globals_dict=globals())
+dag_factory2 = dagfactory.DagFactory(os.path.join(_cur_path,"yaml_loader_kube_example.yaml"))
+
+dag_factory2.clean_dags(globals())
+dag_factory2.generate_dags(globals())
+#from dagfactory import load_yaml_dags
+
+#load_yaml_dags(globals_dict=globals())
