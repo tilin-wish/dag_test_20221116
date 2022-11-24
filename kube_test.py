49b4a0c85618707012a5806cbcc3553b4b2bbcd6
timlin <tilin@wish.com>
2022-11-24 14:49:23 +0800

    test

--- kube_test.py
+++ kube_test.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.operators.dummy import DummyOperator
 from kubernetes.client import CoreV1Api, models as k8s
+from kubernetes.client import V1ResourceRequirements
+
 from airflow.kubernetes.secret import Secret
 
 default_args = {
@@ -22,27 +24,35 @@ dag = DAG(
 
 start = DummyOperator(task_id='start', dag=dag)
 
-passing = KubernetesPodOperator(namespace='airflow',
+simple_kube_task = KubernetesPodOperator(namespace='airflow',
                           image="python:3.6",
                           cmds=["python","-c"],
                           arguments=["print('hello world')"],
                           labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
+                          name="simple-kube-test",
+                          task_id="simple-kube-task",
                           get_logs=True,
                           is_delete_operator_pod=False,
                           #volume_mounts=[],
                           env_vars={"DB_SECRETS_FILE":"/opt/vault/secrets/db_credentials"},
+                          resources=V1ResourceRequirements(
+                            requests={
+                                "cpu": 1,
+                                'memory': '100Mi'
+                            },
+                            limits={
+                                "cpu": 2,
+                                'memory': '200Mi',
+                            })
                           dag=dag
                           )
 
 wishpost_task_test = KubernetesPodOperator(namespace='airflow',
                           image="harbor.infra.wish-cn.com/wish/wishpost@sha256:d9f0eea2fee8634636f0534d7e03d078be22070480ae71b3060c0be562de6db5",
-                          #image="harbor.infra.wish-cn.com/wish/wishpost_payment_pipeline/base@sha256:ed9ee4780189e89c10715327ffe6e6cde7864afe4c35cf412000e7a261619644",
                           cmds=["python"],
                           arguments=["/home/app/wishpost/scripts/test/heavy_memory_test.py"],
                           labels={"foo": "bar"},
-                          name="null-test",
+                          name="wishpost-task-test",
                           task_id="wishpost-task-arg",
                           get_logs=True,
                           is_delete_operator_pod=False,
@@ -54,7 +64,7 @@ wishpost_task_test = KubernetesPodOperator(namespace='airflow',
 
 end = DummyOperator(task_id='end', dag=dag)
 
-#passing.set_upstream(start)
+simple_kube_task.set_upstream(start)
 wishpost_task_test.set_upstream(start)
-#passing.set_downstream(end)
+simple_kube_task.set_downstream(end)
 wishpost_task_test.set_downstream(end)
