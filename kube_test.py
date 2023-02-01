2e99b7dd80e9a51218f09e00cd437c0d5319f56e
timlin <tilin@wish.com>
2023-02-01 15:09:37 +0800

    test

--- kube_test.py
+++ kube_test.py
@@ -10,17 +10,17 @@ from airflow.kubernetes.secret import Secret
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date' : datetime(2023, 1, 25),
+    'start_date' : datetime(2023, 1, 30),
     'schedule_interval': "45 1 * * *",
     'email': ['tilin@wish.com'],
     'email_on_failure': True,
     'email_on_retry': False,
-    'retries': 0,
-    'catchup': False,
+    'retries': 1,
+    'catchup': True,
     'retry_delay': timedelta(minutes=5)
 }
 dag = DAG(
-    'plain_kube2', default_args=default_args)
+    'plain_kube3', default_args=default_args)
 
 start = DummyOperator(task_id='start', dag=dag)
 
@@ -32,7 +32,7 @@ simple_kube_task = KubernetesPodOperator(namespace='airflow',
                           name="simple-kube-test",
                           task_id="simple-kube-task",
                           get_logs=True,
-                          is_delete_operator_pod=True,
+                          is_delete_operator_pod=False,
                           #volume_mounts=[],
                           env_vars={"DB_SECRETS_FILE":"/opt/vault/secrets/db_credentials"},
                           resources=V1ResourceRequirements(
@@ -52,23 +52,19 @@ wishpost_task_test = KubernetesPodOperator(namespace='airflow',
                           image="harbor.infra.wish-cn.com/wish/wishpost-airflow@sha256:803f50dd630ad4e4d8fb218297ecf9fe05b53bcf8195ffb77377cf0744e42355",
                           #image="harbor.infra.wish-cn.com/wish/wishpost-airflow:bff81de-20221226031202_MKL-68756",
                           cmds=["python",
-                          "-c"],
-                          arguments=[
-                            "import time; time.sleep(300);"
-                          ],
-                          #"/home/app/wishpost/scripts/crons/easy_mongo_etl/easy_mongo_etl.py"],
+                          "/home/app/wishpost/scripts/crons/easy_mongo_etl/easy_mongo2s3.py"],
                           #arguments=["/home/app/wishpost/scripts/test/heavy_memory_test.py"],
-                        #   arguments=["--date=20221130T000000",
-                        #    "--c_name=MerchantOrder",
-                        #    "--s3_bucket=wishpost-data",
-                        #     "--task_id=MongoDumpMerchantOrderDaily",
-                        #     "--s3_uri_prefix=tahoe/wishpost/wishpost_merchant_order",
-                        #     "--freq=daily"],
+                          arguments=["--date=20221130T000000",
+                           "--c_name=MerchantOrder",
+                           "--s3_bucket=wishpost-data",
+                            "--task_id=MongoDumpMerchantOrderDaily",
+                            "--s3_uri_prefix=tahoe/wishpost/wishpost_merchant_order",
+                            "--freq=daily"],
                           labels={"foo": "bar"},
                           name="mongo_dump-test",
                           task_id="mongo_dump-task",
                           get_logs=True,
-                          is_delete_operator_pod=True,
+                          is_delete_operator_pod=False,
                           env_vars={"DB_SECRETS_FILE":"/opt/vault/secrets/db_credentials"},
                           #volume_mounts=[k8s.V1VolumeMount(mount_path="/opt/vault/secrets", name="credential")],
                           secrets=[Secret('volume', '/opt/vault/secrets', 'wishflow-wishpost-credential')],
