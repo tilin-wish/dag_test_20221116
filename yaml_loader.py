b2758bcceb2c6cd4500f70d020760507b31b35a3
timlin <tilin@wish.com>
2023-02-16 21:24:23 +0800

    test

--- yaml_loader.py
+++ yaml_loader.py
@@ -2,8 +2,8 @@ from airflow import DAG
 import os
 import dagfactory
 _cur_path = os.path.dirname(os.path.abspath(__file__))
-# #print(_cur_path)
-#dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"mongo_dump.yaml"))
+print(_cur_path)
+dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"dag_mongo_dump.yaml"))
 
 #dag_factory.clean_dags(globals())
 #dag_factory.generate_dags(globals())
@@ -12,6 +12,6 @@ _cur_path = os.path.dirname(os.path.abspath(__file__))
 
 # dag_factory2.clean_dags(globals())
 # dag_factory2.generate_dags(globals())
-from dagfactory import load_yaml_dags
+# from dagfactory import load_yaml_dags
 
-load_yaml_dags(globals_dict=globals(), suffix=['.yaml'])
+# load_yaml_dags(globals_dict=globals(), suffix=['.yaml'])
