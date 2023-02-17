8379b910cb21e75a2c5d001566df731eecbe1441
timlin <tilin@wish.com>
2023-02-17 09:43:05 +0800

    test

--- yaml_loader.py
+++ yaml_loader.py
@@ -5,8 +5,8 @@ _cur_path = os.path.dirname(os.path.abspath(__file__))
 print(_cur_path)
 dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"dag_mongo_dump.yaml"))
 
-#dag_factory.clean_dags(globals())
-#dag_factory.generate_dags(globals())
+dag_factory.clean_dags(globals())
+dag_factory.generate_dags(globals())
 
 # dag_factory2 = dagfactory.DagFactory(os.path.join(_cur_path,"yaml_loader_kube_example.yaml"))
 
