60db215fe9654ace3099f4e67299b2c30f6b64a6
timlin <tilin@wish.com>
2023-04-20 15:28:17 +0800

    test

--- yaml_loader.py
+++ yaml_loader.py
@@ -2,17 +2,9 @@ from airflow import DAG
 import os
 import dagfactory
 _cur_path = os.path.dirname(os.path.abspath(__file__))
-print(_cur_path)
-#dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"dag_mongo_dump.yaml"))
-dag_factory = dagfactory.DagFactory(os.path.join(_cur_path,"dag_cash_flow.yaml"))
 
-dag_factory.clean_dags(globals())
-dag_factory.generate_dags(globals())
+from dagfactory import load_yaml_dags
 
-# dag_factory2 = dagfactory.DagFactory(os.path.join(_cur_path,"yaml_loader_kube_example.yaml"))
-
-# dag_factory2.clean_dags(globals())
-# dag_factory2.generate_dags(globals())
-# from dagfactory import load_yaml_dags
-
-# load_yaml_dags(globals_dict=globals(), suffix=['.yaml'])
+files = os.listdir(os.path.join(_cur_path,"yaml"))
+for file in files:
+    dagfactory.DagFactory(os.path.join(_cur_path, "yaml", file)).generate_dags(globals())
